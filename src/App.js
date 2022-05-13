ffbe50910b986e58371cf9770f16bf73f770513e
royweb <xytank@gmail.com>
2022-05-13 15:43:10 -0400

    Add login with Unstoppable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-
 import './App.css';
 import PianoMaker from './abis/PianoMaker.json';
+import UAuth from '@uauth/js';
+
+const contract_address = "";
+const key = "";
+
+const ud = new UAuth({
+  clientID: "",
+  scope: 'openid email wallet',
+  redirectUri: "http://localhost:3000/",
+})
 
 class App extends Component {
   constructor(props) {
@@ -10,13 +19,14 @@ class App extends Component {
     this.state = {
       account: '',
       contract: null,
-      totalSupply: 0,
-      songs: []
+      songs: [],
+      userData: {}
     }
   }
   async componentWillMount() {
     await this.loadWeb3();
     await this.loadBlockchainData();
+    await this.loadNFTs();
   }
 
   async loadWeb3() {
@@ -36,7 +46,7 @@ class App extends Component {
     const web3 = window.web3;
 
     const accounts = await web3.eth.getAccounts();
-    this.setState({ account: accounts[0] });
+    //this.setState({ account: accounts[0] });
 
     const networkId = await web3.eth.net.getId();
     const networkData = PianoMaker.networks[networkId];
@@ -48,22 +58,34 @@ class App extends Component {
       // Get a copy of the smart contract
       const contract = new web3.eth.Contract(abi, address);
       this.setState({ contract: contract });
-
-      const totalSupply = await contract.methods.totalSupply().call();
-      this.setState({ totalSupply: totalSupply });
-
-      // Load Songs
-      for (let i = 1; i <= totalSupply; i++) {
-        const song = await contract.methods.songs(i - 1).call();
-        this.setState({ songs: [...this.state.songs, song] });
-      }
-
-      console.log(this.state.songs);
     }
     else {
       window.alert('Smart contract not deployed to detected network.');
     }
     
+  }
+
+  async loadNFTs(){
+    let data = await fetch(`https://api.covalenthq.com/v1/42/tokens/${contract_address}/nft_token_ids/?quote-currency=USD&format=JSON&key=${key}`);
+    data = await data.json();
+    console.log(data);
+    let songs = data.data.items;
+    console.log(songs);
+    
+    // Load Songs
+    for (let i = 0; i < songs.length; i++) {
+      let song = await this.state.contract.methods.songs(+songs[i].token_id - 1).call();
+      song = JSON.parse(song);
+      let song_data = {};
+      song_data.value = song.map(s => s.key);
+      song_data.notes = "";
+      song_data.value.forEach(s => {
+        song_data.notes += s + " ";
+      });
+      this.setState({ songs: [...this.state.songs, song_data] });
+    }
+
+    console.log(this.state.songs);
   }
 
   mint = (song) => {
@@ -75,45 +97,68 @@ class App extends Component {
       });
   }
 
+  loginUD = async () => {
+		const user = await ud.loginWithPopup();
+    this.setState({ account: user.idToken.sub});
+	}
+  
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-warning flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="/"
-          >
-            <img className="logo" src="/logo.png" alt="logo" />
-          </a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white"><span id="account">{this.state.account}</span></small>
-            </li>
-          </ul>
+          <div className='container'>
+            <a
+                className="navbar-brand col-sm-3 col-md-2 mr-0"
+                href="/"
+              >
+                <img className="logo" src="/logo.png" alt="logo" />
+              </a>
+              <ul className="navbar-nav px-3">
+                <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+                  <small className="text-white h5"><span id="account">{this.state.account}</span></small>
+                </li>
+              </ul>
+            </div>  
         </nav>
 
-        <div className="container-fluid mt-5">
+        <div className="container mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <h1 className="mt-3">Song List</h1>
-                <button
-                  className="btn btn-warning btn-lg"
-                  onClick={() => this.mint(window.songs)}
-                >
-                  Mint Song as NFT
-                </button>
+                <h1 className="card-title mt-3">Song List</h1>
+                {this.state.account
+                  ? (
+                    <button
+                      className="btn btn-warning btn-lg"
+                      onClick={() => this.mint(window.songs)}
+                    >
+                      Mint Song as NFT
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-primary btn-lg"
+                      onClick={this.loginUD.bind(this)}
+                    >
+                      Login Unstoppable Domain 
+                    </button>
+                  )}
               </div>
             </main>
           </div>
 
           <div className="row text-center">
-            { this.state.songs.map((song, key) => {
+            { this.state.account && this.state.songs.map((song, key) => {
               return(
-                <div key={key} className="col-md-3 mb-3">
-                  <button className="btn btn-success">
-                    Song #{key + 1}
-                  </button>
+                <div key={key} className="col-md-3 mb-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 class="card-title">Song #{key + 1}</h5>
+                      <p className='mb-0'><u>Notes</u></p>
+                      <p>{song.notes}</p>
+                      <a href="#" class="btn btn-warning">Play</a>
+                    </div>
+                  </div>
                 </div>
               )
             })}
